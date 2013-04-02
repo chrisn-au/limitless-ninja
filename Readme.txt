9befb33a2216ca1852e277d0f281cd65d30adaab
Chris N <cneave@gmail.com>
2013-04-03 06:54:11 +1100

    "first commit"

---
+++ Readme.txt
@@ -0,0 +1,6 @@
+
+This module creates 5 instances of the generic 1000 object RGB
+
+The IP addrress is hard coded in lib/limitlesswhite
+
+Need to find these blocks in the cloud and rename
---
+++ index.js
@@ -0,0 +1,106 @@
+var LimitlessWhite = require('./lib/limitlesswhite')
+  , util = require('util')
+  , configHandlers = require('./lib/config')
+  , url = require('url')
+  , events = require('events')
+  , stream = require('stream');
+
+var LimitlessLEDRGB = require('./lib/LimitlessLEDRGB')
+  , util = require('util')
+  , stream = require('stream');
+  
+// Give our module a stream interface
+util.inherits(myModule,stream);
+
+/**
+ * Called when our client starts up
+ * @constructor
+ *
+ * @param  {Object} opts Saved/default module configuration
+ * @param  {Object} app  The app event emitter
+ * @param  {String} app.id The client serial number
+ *
+ * @property  {Function} save When called will save the contents of `opts`
+ * @property  {Function} config Will be called when config data is received from the cloud
+ *
+ * @fires register - Emit this when you wish to register a device (see Device)
+ * @fires config - Emit this when you wish to send config data back to the cloud
+ */
+function myModule(opts,app) {
+
+  var self = this;
+  this.first = true;
+  this._opts = opts;
+  
+  console.log(opts);
+
+  app.on('client::up',function(){
+
+    // The client is now connected to the cloud
+
+    // Do stuff with opts, and then commit it to disk
+    if (!opts.hasMutated) {
+      opts.hasMutated = true;
+    }
+
+    self.save();
+    if (self.first)
+    {
+        console.log(self._opts.urls.length);
+        
+        self._opts.urls.forEach(function(url,index)
+         {
+            var n = url.split('.');
+            var eventEmitter = new events.EventEmitter();
+            self.emit('register', new LimitlessLEDRGB(url,n[3]));
+            
+            for(var i = 0; i < 5; i++) {
+               light =  new LimitlessWhite(i,url,n[3],index,eventEmitter)
+               self.emit('register', light ); 
+            }          
+         }); 
+ 
+       self.first = false;
+    }   
+  //  self.emit('
+  });
+};
+
+/**
+ * Called when config data is received from the cloud
+ * @param  {Object} config Configuration data
+ */
+myModule.prototype.config = function(config) {
+
+};
+
+myModule.prototype.updateAll = function(index,state) {
+         
+};
+
+myModule.prototype.config = function(rpc,cb) {
+
+  var self = this;
+
+  if (!rpc) {
+    return configHandlers.probe.call(this,cb);
+  }
+
+  switch (rpc.method) {
+    case 'manual_set_url':     return configHandlers.manual_set_url.call(this,rpc.params,cb); break;
+    case 'manual_get_url':     return configHandlers.manual_get_url.call(this,rpc.params,cb); break;
+    case 'manual_show_remove': return configHandlers.manual_show_remove.call(this,rpc.params,cb); break;
+    case 'manual_remove_url': return configHandlers.manual_remove_url.call(this,rpc.params,cb); break;
+
+    default:                   return cb(true);                                              break;
+  }
+};
+
+myModule.prototype.createswitchbyip = function(ipaddress,index) {
+
+  
+};
+
+// Export it
+
+module.exports = myModule;
